791a5e998200923c37028c35729a3de0cdd174c9
AsadullahBadhan <asadullah.badhan@gmail.com>
2023-07-16 20:41:52 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+ *Title: Random Quote generator
+ *Description: Simple node application that print random quotes per second interval.
+ *Author: Asadullah Badhan inspired by sumit saha.
+ *Date: 07/16/2023
+ *
+ */
+
+// Dependencies
+
+// App object - Module Scaffolding
+const app = {};
+
+// Configuration
+app.config = {
+    timeBetweenQuotes: 1000,
+};
